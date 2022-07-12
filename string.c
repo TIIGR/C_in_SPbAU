a7463cf1ca3405ac62cc6ab97316c64c0e37423b
♫♣MΩTΩRΩLѦ♣♫ <55460474+TIIGR@users.noreply.github.com>
2022-07-12 18:42:25 +0300

    update

---
+++ string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int strlen2(char *str)
+{
+    int c = 0;
+    while (str[c] != '\0') {
+        c++;
+    }
+    return c;
+}
+
+int main()
+{
+    char str[128];
+    printf("Enter string: ");
+    gets(str);
+    printf("strlen() result = %i\n", strlen(str));
+    printf("strlen2() result = %i\n", strlen2(str));
+    return 0;
+}
