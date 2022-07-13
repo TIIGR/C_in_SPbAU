fe17a6c85e5389cec6617bbf5646ce0bd7922890
♫♣MΩTΩRΩLѦ♣♫ <55460474+TIIGR@users.noreply.github.com>
2022-07-13 13:58:38 +0300

    update

---
+++ arithmetic.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main() {
+    float a, b; char sign;
+    printf("Welcome to arithmetic calculator! List of available operations: {+,-,*,/,^}\n");
+    printf("Enter your arithmetic opetation on two numbers here (for example: 2 + 3):");
+    scanf("%f %c %f", &a, &sign, &b);
+    if (sign == '0') {
+        printf("Bye");
+        return 0;
+    } else if (sign == '+') {
+        printf("%f", a + b);
+        return 0;
+    } else if (sign == '-') {
+        printf("%f", a - b);
+        return 0;
+    } else if (sign == '*') {
+        printf("%f", a * b);
+        return 0;
+    } else if (sign == '/') {
+        if (b == 0) {
+            printf("Incorrect data!\n");
+            return -1;
+        } else {
+            printf("%f", a / b);
+            return 0;
+        }
+    } else if (sign == '^') {
+        if (a < 0) {
+            printf("Incorrect data!\n");
+            return -1;
+        }  else {
+            printf("%f", pow(a, b));
+            return 0;
+        }
+    } else {
+        printf("Incorrect data!\n");
+        return -1;
+    }
+}
